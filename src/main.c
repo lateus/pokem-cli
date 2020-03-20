19b39aed8ef440c60253501a87efedd6d2141ce6
Carlos Enrique Pérez Sánchez <thecrowporation@gmail.com>
2020-03-19 22:07:10 -0400

    Update copyright notice

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ int main(int argc, const char *argv[])
 
     /* Copyright notice */
     fputs(LIGHT PROGRAM_STRING RESET DRED " v" VERSION_STRING DGREEN "   Copyright 2018-2020 Carlos Enrique Perez Sanchez.\n"
-          "..............................................................\n", stdout);
+          "..............................................................\n\n", stdout);
     fflush(stdout);
 
     int i; /* iterations */
